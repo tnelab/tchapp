4a7d952ea1a6649b954a4862e16d8e00e33068b9
esrin <esrin@qq.com>
2016-12-13 20:54:11 +0800

    删除simple目录，添加example目录

--- src/example/TchApp.Example.Hello/Program.cs
+++ src/example/TchApp.Example.Hello/Program.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using TchApp.TchClient;
 
-namespace TchApp.Simple.Hello
+namespace TchApp.Example.Hello
 {
     public class Program
     {
